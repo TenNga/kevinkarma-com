a0b0aaeb061d22e726dd294a2aa04369e5e4a14a
Kevin Karma <tenngawang6080@gmail.com>
2024-12-11 19:22:53 -0500

    added some  small animation

--- scroll.js
+++ scroll.js
@@ -4,4 +4,13 @@ $(document).ready(()=>{
         origin: 'bottom',
         opacity: null
     });
+
+
+    const textWords = document.querySelectorAll('.bio-bullet');
+
+textWords.forEach((word, index) => {
+  const delay = index * 1; // Adjust delay as needed
+//   word.style.animation = `highlight 1s ease-in-out ${delay}s infinite`;
+});
+
 })
